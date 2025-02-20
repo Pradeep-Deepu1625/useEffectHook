45db0125510b2677b4978cec1e3b6eafed7df014
Deepu <pradeep9100234360@gmail.com>
2025-02-20 07:16:49 +0530

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+
 function App(){
+  const [counter, setCounter] = useState(0)
+  // This useEffect will be called when ever the state get updated or re-rendered this non- dependency useeffect will be called
+  useEffect(()=>{
+    console.log('NON-Dependency Array ==>This will be called when mounting time as well reload time, updation time')
+  })
+  // Empty - Dependency Array
+  useEffect(()=>{
+    console.log("Empty Dependency Array ===> this useEffect will be called only once on mounting time after UI rendered")
+  },[]) 
+  //updation
+  useEffect(()=>{
+    console.log('updation Time ===> This useEffect will we called when that particular state get updated or changed')
+  },[counter])
+  let inc = ()=>{
+    setCounter(()=> counter + 1)
+  }
+  let dec = ()=>{
+    setCounter(()=> counter - 1)
+  }
   return(
     <div>
       <h1>We are working with useEffect Hook</h1>
+      <h2>Counter : {counter}</h2>
+      <button onClick={()=> inc()}>Increament</button>
+      <button onClick={()=>dec()}>Decreament</button>
+
     </div>
   )
 }
